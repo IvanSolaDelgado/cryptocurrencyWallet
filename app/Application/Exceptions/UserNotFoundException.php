b00fbbaa904dabc4f071f4847988ec85eee4932b
Ivan Sola <soladelgadoivan@gmail.com>
2023-08-02 11:23:11 +0200

    [20] - Minor refactor naming

--- app/Application/Exceptions/UserNotFoundException.php
+++ app/Application/Exceptions/UserNotFoundException.php
@@ -3,11 +3,12 @@
 namespace App\Application\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserNotFoundException extends Exception
 {
     public function __construct()
     {
-        parent::__construct('User not found', 200);
+        parent::__construct('User not found', Response::HTTP_OK);
     }
 }
