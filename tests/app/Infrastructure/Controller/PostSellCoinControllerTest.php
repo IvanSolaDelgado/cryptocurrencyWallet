7b2211e546a26e624b19be84e22347b8398d6680
Ivan Sola <soladelgadoivan@gmail.com>
2023-08-08 13:09:12 +0200

    [47] - Integration tests do not mock WalletDataSource

--- tests/app/Infrastructure/Controller/PostSellCoinControllerTest.php
+++ tests/app/Infrastructure/Controller/PostSellCoinControllerTest.php
@@ -4,8 +4,7 @@ namespace Tests\app\Infrastructure\Controller;
 
 use App\Domain\Coin;
 use App\Domain\DataSources\CoinDataSource;
-use App\Domain\DataSources\WalletDataSource;
-use App\Domain\Wallet;
+use Illuminate\Support\Facades\Cache;
 use Mockery;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
@@ -13,103 +12,110 @@ use Tests\TestCase;
 class PostSellCoinControllerTest extends TestCase
 {
     private CoinDataSource $coinDataSource;
-    private WalletDataSource $walletDataSource;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->coinDataSource = Mockery::mock(CoinDataSource::class);
-        $this->walletDataSource = Mockery::mock(WalletDataSource::class);
         $this->app->bind(CoinDataSource::class, function () {
             return $this->coinDataSource;
         });
-        $this->app->bind(WalletDataSource::class, function () {
-            return $this->walletDataSource;
-        });
     }
 
     /**
      * @test
      */
-    public function ifBadRequestThrowsError()
-    {
-        $response = $this->post('api/coin/sell', ["coin_id" => null,
-                                                    "wallet_id" => null,
-                                                    "amount_usd" => null]);
-
-        $response->assertStatus(Response::HTTP_BAD_REQUEST);
-        $response->assertExactJson(['description' => 'bad request error']);
-    }
-
-    /**
-     * @test
-     */
-    public function ifCoinIdNotFoundThrowsError()
+    public function coinIdWasNotFoundWhenCoinDoesNotExist()
     {
         $this->coinDataSource
             ->expects("findById")
             ->with("coin_id_value", "1")
             ->andReturn(null);
 
-        $response = $this->post('api/coin/sell', ["coin_id" => "coin_id_value",
-            "wallet_id" => "wallet_id_value",
-            "amount_usd" => 1]);
+        $response = $this->post(
+            'api/coin/sell',
+            [
+                "coin_id" => "coin_id_value",
+                "wallet_id" => "wallet_id_value",
+                "amount_usd" => 1
+            ]
+        );
 
         $response->assertStatus(Response::HTTP_NOT_FOUND);
-        $response->assertExactJson(['description' => 'A coin with the specified ID was not found']);
+        $response->assertExactJson(
+            [
+                'description' => 'A coin with the specified ID was not found'
+            ]
+        );
     }
 
     /**
      * @test
      */
-    public function ifWalletIdNotFoundThrowsError()
+    public function walletIdWasNotFoundWhenWalletDoesNotExist()
     {
+        $coin = new Coin("coin_id_value", "name_value", "symbol_value", 1, 1);
+
         $this->coinDataSource
             ->expects("findById")
             ->with("coin_id_value", "1")
-            ->andReturn(new Coin("coinId", "name", "symbol", 2.0, 1.0));
-        $this->walletDataSource
-            ->expects("findById")
-            ->with("walletId")
-            ->andReturn(null);
+            ->andReturn($coin);
+        Cache::shouldReceive('has')->once()->with('wallet_0')->andReturn(false);
 
-        $response = $this->post('api/coin/sell', ["coin_id" => "coin_id_value",
-            "wallet_id" => "walletId",
-            "amount_usd" => 1]);
-
+        $response = $this->post(
+            'api/coin/sell',
+            [
+                "coin_id" => "coin_id_value",
+                "wallet_id" => "0",
+                "amount_usd" => 1
+            ]
+        );
 
         $response->assertStatus(Response::HTTP_NOT_FOUND);
-        $response->assertExactJson(['description' => 'A wallet with the specified ID was not found']);
+        $response->assertExactJson(
+            [
+                'description' => 'A wallet with the specified ID was not found'
+            ]
+        );
     }
 
     /**
      * @test
      */
-    public function ifCoinWasSoldCorrectlyReturnSuccessful()
+    public function sellsCoinWhenCoinAndWalletExist()
     {
+        $coin = new Coin("coin_id_value", "name_value", "symbol_value", 1, 1);
+
         $this->coinDataSource
             ->expects("findById")
             ->with("coin_id_value", "1")
-            ->andReturn($coin = new Coin(
-                "coin_id_value",
-                "name_value",
-                "symbol_value",
-                1,
-                1
-            ));
-        $this->walletDataSource
-            ->expects("findById")
-            ->with("walletId")
-            ->andReturn(new Wallet("walletId"));
-        $this->walletDataSource
-            ->expects("sellCoinFromWallet")
-            ->with("walletId", $coin, 1);
+            ->andReturn($coin);
+        Cache::shouldReceive('has')->once()->with('wallet_0')->andReturn(true);
+        Cache::shouldReceive('has')->once()->with('wallet_0')->andReturn(true);
+        Cache::shouldReceive('get')->once()->with("wallet_0")->andReturn(
+            [
+                'walletId' => '0',
+                'BuyTimeAccumulatedValue' => 0,
+                'coins' => [],
+            ]
+        );
+        Cache::shouldReceive('put')
+            ->with("wallet_0", Mockery::type('array'));
 
-        $response = $this->post('api/coin/sell', ["coin_id" => "coin_id_value",
-            "wallet_id" => "walletId",
-            "amount_usd" => 1]);
+        $response = $this->post(
+            'api/coin/sell',
+            [
+                "coin_id" => "coin_id_value",
+                "wallet_id" => "0",
+                "amount_usd" => 1
+            ]
+        );
 
         $response->assertStatus(Response::HTTP_OK);
-        $response->assertExactJson(['description' => 'successful operation']);
+        $response->assertExactJson(
+            [
+                'description' => 'successful operation'
+            ]
+        );
     }
 }
