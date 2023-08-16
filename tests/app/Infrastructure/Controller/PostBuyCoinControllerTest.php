10dd0e7fdbf0104f07e0e84730187d050b7aaf54
Ivan Sola <113472661+IvanSolaDelgado@users.noreply.github.com>
2023-08-16 08:45:04 +0200

    [34] - Integration tests do not use WalletDataSource mocks
    * [34] - Integration tests do not use WalletDataSource mocks
    
    * [35] - Form request used to validate request params
    
    * [35] - Form request used to validate request params
    
    * [35] - BuyCoinRequest tested
    
    * [36] - Separate controller responsabilities
    
    * [36] - Service created]
    
    * [36] - Controller responsability extracted
    
    * [36] - BuyCoinService tested
    
    * [36] - Bad request changed to not found
    
    * [35] - Comment deleted
    
    * [34] - Minor refactor on PostBuyControllerTest

--- tests/app/Infrastructure/Controller/PostBuyCoinControllerTest.php
+++ tests/app/Infrastructure/Controller/PostBuyCoinControllerTest.php
@@ -4,70 +4,55 @@ namespace Tests\app\Infrastructure\Controller;
 
 use App\Domain\Coin;
 use App\Domain\DataSources\CoinDataSource;
-use App\Domain\DataSources\WalletDataSource;
-use App\Domain\Wallet;
+use Illuminate\Support\Facades\Cache;
 use Mockery;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class PostBuyCoinControllerTest extends TestCase
 {
     private CoinDataSource $coinDataSource;
-    private WalletDataSource $walletDataSource;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->coinDataSource = Mockery::mock(CoinDataSource::class);
-        $this->walletDataSource = Mockery::mock(WalletDataSource::class);
         $this->app->bind(CoinDataSource::class, function () {
             return $this->coinDataSource;
         });
-        $this->app->bind(WalletDataSource::class, function () {
-            return $this->walletDataSource;
-        });
     }
 
     /**
      * @test
      */
-    public function ifCoinIdNotFoundThrowsError()
+    public function coinIdWasNotFoundWhenCoinDoesNotExist()
     {
         $this->coinDataSource
             ->expects("findById")
             ->with("coin_id_value", "1")
             ->andReturn(null);
 
-        $response = $this->post('api/coin/buy', ["coin_id" => "coin_id_value",
-                                                      "wallet_id" => "wallet_id_value",
-                                                      "amount_usd" => 1]);
+        $response = $this->post(
+            'api/coin/buy',
+            [
+                "coin_id" => "coin_id_value",
+                "wallet_id" => "wallet_id_value",
+                "amount_usd" => 1
+            ]
+        );
 
-        $response->assertNotFound();
-        $response->assertExactJson(['description' => 'A coin with the specified ID was not found.']);
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+        $response->assertExactJson(
+            [
+                'description' => 'Coin not found'
+            ]
+        );
     }
 
     /**
      * @test
      */
-    public function ifBadRequestThrowsError()
-    {
-        $this->coinDataSource
-            ->expects("findById")
-            ->with("coin_id_value", "1")
-            ->times(0)
-            ->andReturn(null);
-
-        $response = $this->post('api/coin/buy', ["coin_id" => null,
-                                                    "wallet_id" => null,
-                                                    "amount_usd" => null]);
-
-        $response->assertBadRequest();
-        $response->assertExactJson(['description' => 'bad request error']);
-    }
-
-    /**
-     * @test
-     */
-    public function ifCoinWasBoughtCorrectlyReturnSuccessful()
+    public function coinIdWasNotFoundWhenWalletDoesNotExist()
     {
         $this->coinDataSource
             ->expects("findById")
@@ -79,18 +64,61 @@ class PostBuyCoinControllerTest extends TestCase
                 1,
                 1
             ));
-        $this->walletDataSource
+        Cache::shouldReceive('has')->once()->with('wallet_0')->andReturn(false);
+
+        $response = $this->post(
+            'api/coin/buy',
+            [
+                "coin_id" => "coin_id_value",
+                "wallet_id" => "0",
+                "amount_usd" => 1
+            ]
+        );
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+        $response->assertExactJson(
+            [
+                'description' => 'Wallet not found'
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function successfullyBuyOperation()
+    {
+        $coin = new Coin("coin_id_value", "name_value", "symbol_value", 1, 1);
+        $this->coinDataSource
             ->expects("findById")
-            ->with("wallet_id_value")
-            ->andReturn(new Wallet("wallet_id_value"));
-        $this->walletDataSource
-            ->expects("insertCoinInWallet");
+            ->with("coin_id_value", "1")
+            ->andReturn($coin);
+        Cache::shouldReceive('has')->once()->with('wallet_0')->andReturn(true);
+        Cache::shouldReceive('has')->once()->with('wallet_0')->andReturn(true);
+        Cache::shouldReceive('get')->once()->with("wallet_0")->andReturn(
+            [
+                'walletId' => '0',
+                'BuyTimeAccumulatedValue' => 0,
+                'coins' => [],
+            ]
+        );
+        Cache::shouldReceive('put')
+            ->with("wallet_0", Mockery::type('array'));
 
-        $response = $this->post('api/coin/buy', ["coin_id" => "coin_id_value",
-                                                    "wallet_id" => "wallet_id_value",
-                                                    "amount_usd" => 1]);
+        $response = $this->post(
+            'api/coin/buy',
+            [
+                "coin_id" => "coin_id_value",
+                "wallet_id" => "0",
+                "amount_usd" => 1
+            ]
+        );
 
-        $response->assertOk();
-        $response->assertExactJson(['description' => 'successful operation']);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertExactJson(
+            [
+                'description' => 'successful operation'
+            ]
+        );
     }
 }
